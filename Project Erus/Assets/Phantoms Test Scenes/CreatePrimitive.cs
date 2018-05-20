08864d14c24dd71e697ed62e7440779b9ed1c703
Phantom5522 <phantom.5522@gmx.de>
2018-05-20 21:26:39 +0200

    Add CreatePrimitve-Script

---
+++ Project Erus/Assets/Phantoms Test Scenes/CreatePrimitive.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CreatePrimitive : MonoBehaviour {
+
+    public PrimitiveType pType = PrimitiveType.Cube;
+
+    public float x = 0;
+    public float y = 0;
+    public float z = 0;
+
+    // Use this for initialization
+    void Start () {
+
+        GameObject pObkect = GameObject.CreatePrimitive(pType);
+
+        pObkect.transform.position = new Vector3(x, y, z);
+
+        pObkect.name = "Generated " + pType.ToString();
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+
+
+	}
+}
