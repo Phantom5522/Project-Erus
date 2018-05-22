5f8d67fd146c3f92bf1f8aec5b8acbc80567daa1
Phantom5522 <phantom.5522@gmx.de>
2018-05-22 13:42:02 +0200

    add "Main Menu"-Scene & edit CreatePrimitive

--- Project Erus/Assets/Phantoms Test Scenes/CreatePrimitive.cs
+++ Project Erus/Assets/Phantoms Test Scenes/CreatePrimitive.cs
@@ -5,19 +5,28 @@ using UnityEngine;
 public class CreatePrimitive : MonoBehaviour {
 
     public PrimitiveType pType = PrimitiveType.Cube;
+    public bool addSinMove;
 
     public float x = 0;
     public float y = 0;
     public float z = 0;
 
     // Use this for initialization
-    void Start () {
+    void Start() {
 
         GameObject pObject = GameObject.CreatePrimitive(pType);
 
         pObject.transform.position = new Vector3(x, y, z);
 
         pObject.name = "Generated " + pType.ToString();
+
+
+        if (addSinMove)
+        {
+            SinMove pSin = pObject.AddComponent<SinMove>();
+            pSin.amplitude = 0.5f;
+            pSin.speed = 0.1f;
+        }
 
 	}
 	
