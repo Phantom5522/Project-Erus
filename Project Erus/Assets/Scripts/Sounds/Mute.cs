49d33ca75ec2e672c103c8d55d33d2e97e7396be
Phantom5522 <phantom.5522@gmx.de>
2018-05-23 02:31:56 +0200

    Add Test-Menu-Scene, Mute-Script, Fonts, Directory-Structure

---
+++ Project Erus/Assets/Scripts/Sounds/Mute.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+using UnityEngine.UI;
+
+public class Mute : MonoBehaviour, IPointerDownHandler
+{
+    // Inspector
+    public bool isMuted = false;
+    public AudioSource soundSource;
+    public Sprite mutedSprite;
+
+    // private
+    Image image;
+    Sprite saveSprite;
+
+    // Use this for initialization
+    void Start()
+    {
+        // Get Image and Save the current Sprite
+        image = GetComponent<Image>();
+        saveSprite = image.sprite;
+
+        if (isMuted)
+            SpeakerOff();
+	}
+
+    void IPointerDownHandler.OnPointerDown(PointerEventData eventData)
+    {
+        isMuted = !isMuted;
+
+        if (isMuted)
+            SpeakerOff();
+        else
+            SpeakerOn();
+
+    }
+
+    private void SpeakerOn()
+    {
+        image.sprite = saveSprite;
+        soundSource.mute = false;
+    }
+
+    private void SpeakerOff()
+    {
+        image.sprite = mutedSprite;
+        soundSource.mute = true;
+    }
+
+}
