62e8c0cab4fc9ffb69c60f76098099eeac472cdc
Phantom5522 <phantom.5522@gmx.de>
2018-05-23 15:39:44 +0200

    Changed to Responsive Menu

---
+++ Project Erus/Assets/Scripts/Menu/MainMenu.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+using UnityEngine.SceneManagement;
+
+public class MainMenu : MonoBehaviour
+{
+
+    public void ExitGame()
+    {
+        Application.Quit();
+        Debug.Log("Test");
+    }
+
+    public void NewGame()
+    {
+        SceneManager.LoadScene("Introduction");
+    }
+
+}
