698ba79a1336bf8db496a4cc8ec23747846e1f59
Phantom5522 <phantom.5522@gmx.de>
2018-05-23 17:23:19 +0200

    New Menu Features
    - add sub Menus

--- Project Erus/Assets/Scripts/Menu/MainMenu.cs
+++ Project Erus/Assets/Scripts/Menu/MainMenu.cs
@@ -6,16 +6,40 @@ using UnityEngine.SceneManagement;
 
 public class MainMenu : MonoBehaviour
 {
+    public GameObject optionsPanel;
+    public GameObject startPanel;
+
+    public void Show(GameObject g)
+    {
+        if (!g.activeSelf)
+        {
+            DeactivateAll();
+            g.SetActive(true);
+        }
+        else
+            DeactivateAll();
+    }
+
+    // Deaktiviert alle unter Menüs
+    public void DeactivateAll()
+    {
+        optionsPanel.SetActive(false);
+        startPanel.SetActive(false);
+    }
+
+
+    public void NewGame()
+    {
+        DeactivateAll();
+        SceneManager.LoadScene("Introduction");
+    }
 
     public void ExitGame()
     {
+        DeactivateAll();
         Application.Quit();
         Debug.Log("Test");
     }
 
-    public void NewGame()
-    {
-        SceneManager.LoadScene("Introduction");
-    }
 
 }
