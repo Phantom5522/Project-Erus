ba1bfe3b1d24e82a2bf35db860c83627a6b4c667
Phantom5522 <phantom.5522@gmx.de>
2018-05-23 23:50:23 +0200

    Add Master Volume Function

---
+++ Project Erus/Assets/Scripts/Menu/OptionsMenu.cs
@@ -0,0 +1,58 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class OptionsMenu : MonoBehaviour {
+
+    public AudioSource audioOutput;
+    public Slider masterVolumeSlider;
+    public Text masterVolumeValue;
+
+    private void Start()
+    {
+        #region Start Configure Graphics
+
+        ChangedMasterVolume();
+
+
+        #endregion
+
+        #region Start Configure Audio
+
+
+        #endregion
+
+        #region Start Configure Keyboard
+
+
+        #endregion
+
+
+    }
+
+    #region Configure Graphics
+
+
+    #endregion
+
+    #region Configure Audio
+
+    public void ChangedMasterVolume()
+    {
+
+        masterVolumeValue.text = masterVolumeSlider.value.ToString();
+
+        audioOutput.volume = masterVolumeSlider.value / 100;
+
+    }
+
+    #endregion
+
+    #region Configure Keyboard
+
+
+    #endregion
+
+
+}
