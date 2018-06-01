4a7d7b65ab96ea176c62c7ddb9022dcac6803a1c
Phantom5522 <phantom.5522@gmx.de>
2018-06-01 17:37:43 +0200

    Player Added in Introduction Scene

---
+++ Project Erus/Assets/Scripts/Player/CatController.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CatController : MonoBehaviour {
+
+
+    bool isWalking;
+    bool isRunning;
+	
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
