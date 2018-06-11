fb8520322350cc168bf7c6a04a18447f6885a71a
Phantom5522 <phantom.5522@gmx.de>
2018-06-11 03:09:42 +0200

    Added Blur

--- Project Erus/Assets/Scripts/Menu/InGameUI.cs
+++ Project Erus/Assets/Scripts/Menu/InGameUI.cs
@@ -1,19 +1,26 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.PostProcessing;
 
 public class InGameUI : MonoBehaviour {
 
     public GameObject menuCanvas;
+    public Camera camera;
+
+    // Profiles for PostProcessing
+    public PostProcessingProfile normalProfile;
+    public PostProcessingProfile menuProfile;
 
     [HideInInspector]
     public bool inMenu = false;
 
-	void Start () {
+	void Start() {
 		
 	}
 	
-	void FixedUpdate () {
+	void FixedUpdate()
+    {
 
         if (Input.GetKeyUp(KeyCode.Escape))
         {
@@ -21,8 +28,19 @@ public class InGameUI : MonoBehaviour {
 
             menuCanvas.SetActive(!menuCanvas.activeSelf);
 
+            updatePostProcessing();
+
         }
 
+    }
+
+    void updatePostProcessing()
+    {
+        if (inMenu)
+            camera.GetComponent<PostProcessingBehaviour>().profile = menuProfile;
+        else
+            camera.GetComponent<PostProcessingBehaviour>().profile = normalProfile;
 
     }
+
 }
