bafb4847e0fe256bbeb7836e23fec53a1d3dc65b
Phantom5522 <phantom.5522@gmx.de>
2018-06-13 03:39:41 +0200

    Added anit elastic camera movement

---
+++ Project Erus/Assets/Scripts/World/MirroringXPos.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MirroringXPos : MonoBehaviour {
+
+    public GameObject player;
+    public GameObject followEmpty;
+	
+	// Update is called once per frame
+	void Update () {
+
+        float addDistance = (player.transform.position.x - followEmpty.transform.position.x)*2;
+
+        transform.position = new Vector3(followEmpty.transform.position.x + addDistance, followEmpty.transform.position.y, -10f);
+
+	}
+}
