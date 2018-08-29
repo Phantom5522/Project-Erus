e25ff39c6d35823dd9cd2a45122e88ed388a1f0d
Phantom5522 <phantom.5522@gmx.de>
2018-08-29 19:24:59 +0200

    Added Combo Attack Animation

---
+++ Project Erus/Assets/Scripts/Player/Weapon.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+class Weapon : MonoBehaviour
+{
+    public string animationInteger;     // Animator-Variable to switch the animation
+    public int comboLength;             // How many Combosattacks
+
+}
--- Project Erus/Assets/Scripts/Player/WeaponSystem.cs
+++ Project Erus/Assets/Scripts/Player/WeaponSystem.cs
@@ -4,24 +4,31 @@ using UnityEngine;
 
 public class WeaponSystem : MonoBehaviour {
 
+    public Animator ani;
+
     public GameObject weaponBone;
     public GameObject[] weaponPrefabs;
 
     private GameObject curWeapon;
     private int curWeaponIndex = 0;
 
-	// Use this for initialization
-	void Start () {
+    // Attack
+    private int comboCounter = 0;
+    private bool attackWasReleased = true;
 
+    // Use this for initialization
+    void Start () {
+
+        ani = GetComponent<Animator>();
 
         switchWeapon(curWeaponIndex);
-
 
     }
 	
 	// Update is called once per frame
 	void Update () {
 
+        // scroll Weapon forward
         if (Input.mouseScrollDelta.y > 0)
         {
             if (curWeaponIndex + 1 >= weaponPrefabs.Length)
@@ -30,6 +37,7 @@ public class WeaponSystem : MonoBehaviour {
                 switchWeapon(curWeaponIndex + 1);
 
         }
+        // scroll Weapon backwards
         else if (Input.mouseScrollDelta.y < 0)
         {
             if (curWeaponIndex - 1 < 0)
@@ -38,16 +46,71 @@ public class WeaponSystem : MonoBehaviour {
                 switchWeapon(curWeaponIndex - 1);
         }
 
+        #region Attack-Inputs
 
+        Debug.Log(comboCounter);
+
+        if (Input.GetKey(KeyCode.L) && attackWasReleased)
+        {
+            attackWasReleased = false;
+
+            if (curWeapon.GetComponent<Weapon>().comboLength > comboCounter)
+            {
+                Debug.Log(curWeapon.GetComponent<Weapon>().comboLength + " <> " + (comboCounter + 1));
+                comboCounter++;
+                PlayAnimation();
+                StartCoroutine(ResetCombo(comboCounter, 0.5F));
+            }
+        }
+
+        if (!Input.GetKey(KeyCode.L))
+            attackWasReleased = true;
+
+        #endregion
+
+    }
+
+    private IEnumerator ResetCombo(int comboCounter, float delayTime)
+    {
+        yield return new WaitForSeconds(delayTime);
+
+        Debug.Log(comboCounter + " | " +  this.comboCounter);
+
+        if (this.comboCounter == comboCounter)
+        {
+            this.comboCounter = 0;
+            StopAnimation();
+        }
     }
 
     private void switchWeapon(int index) {
 
-        Destroy(curWeapon);
+        if (curWeapon != null)
+            StopAnimation();
 
+        Destroy(curWeapon); // Destroy current Weaponobject
+
+
+        // create new Weapon objekt
         curWeaponIndex = index;
         curWeapon = Instantiate(weaponPrefabs[index]);
         curWeapon.transform.SetParent(weaponBone.transform, false);
+
+    }
+
+    private void PlayAnimation()
+    {
+        Weapon weaponPorps = curWeapon.GetComponent<Weapon>();
+
+        ani.SetInteger(weaponPorps.animationInteger, comboCounter);
+
+    }
+
+    private void StopAnimation()
+    {
+        Weapon weaponPorps = curWeapon.GetComponent<Weapon>();
+
+        ani.SetInteger(weaponPorps.animationInteger, 0);
 
     }
 
